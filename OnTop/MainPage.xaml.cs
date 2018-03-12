e5026aba52ac305b1093993c11c3f97f188bf3a0
Kevin Coughlin <kevintcoughlin@gmail.com>
2018-03-11 21:09:03 -0700

    init

---
+++ OnTop/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OnTop
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            bool isSupported = ApplicationView.GetForCurrentView().IsViewModeSupported(ApplicationViewMode.CompactOverlay);
+            Debug.WriteLine("Is supported: " + isSupported);
+
+            SetViewModeAsync();
+        }
+
+        private async System.Threading.Tasks.Task SetViewModeAsync()
+        {
+            bool modeSwitched = await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay);
+            Debug.WriteLine("Mode switched: " + modeSwitched);
+        }
+    }
+}
